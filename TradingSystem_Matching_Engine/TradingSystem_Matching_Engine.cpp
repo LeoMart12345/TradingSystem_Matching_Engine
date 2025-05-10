4cce00dfc2b91e6b454e94d87e3f880670c19804
LeoMart12345 <leomartinezhz@gmail.com>
2025-05-10 23:11:02 +0100

    basic order class with enum for buy or sell

---
+++ TradingSystem_Matching_Engine/TradingSystem_Matching_Engine.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <memory>
+#include <map>
+
+enum Type {
+   BUY,
+   SELL
+};
+
+class order {
+
+public: 
+	Type BuyOrSell;
+	int mVolume;
+	std::string mName;
+
+
+
+private:
+
+
+};
