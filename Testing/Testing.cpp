01ff031ac19b573cbcb35996169cd322b3668858
Leo Martinez <l.copemartinez1@universityofgalway.ie>
2025-10-05 19:10:37 +0100

    Testing cpp Features

---
+++ Testing/Testing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+#include <vector>
+ 
+int main(){
+    int x = 53;
+
+    int& ref = x;
+    // refs need to reference somthing at compile time.
+    // you cant reference somthing that is null
+ 
+}
+
--- TradingSystem_Matching_Engine/Client.cpp
+++ TradingSystem_Matching_Engine/Client.cpp
@@ -1,21 +1,19 @@
 #include  <iostream>
 
-//
-//int main() {
-//	std::cout << "Hello, MR CLIENT SIR!" << std::endl;
-//
-//
-//	while (1) {
-//
-//		char buffer[1024]{};
-//		std::cout << "Enter command: ";
-//		std::cin >> buffer;
-//
-//		std::cout << "you entered: " << buffer << std::endl;
-//		return 0;
-//	}
-//
-//
-//
-//	return 0;
-//}
+
+int main() {
+	std::cout << "Hello, MR CLIENT SIR!" << std::endl;
+
+
+	while (1) {
+
+		char buffer[1024]{};
+		std::cout << "Enter command: ";
+		std::cin >> buffer;
+
+		std::cout << "you entered: " << buffer << std::endl;
+		return 0;
+	}
+
+	return 0;
+}
