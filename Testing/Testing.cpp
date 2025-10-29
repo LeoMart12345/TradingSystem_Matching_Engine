ce9c75788ec4d324ae8fe6d8cae08b3ec0ef7a68
Leo Martinez <l.copemartinez1@universityofgalway.ie>
2025-10-29 17:50:59 +0000

    Testing some dynamic programming concepts

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -3,24 +3,19 @@
 #include <vector>
 #include <memory>
 
-class Dog{
-
-    public:
-        void bark(){
-            std::cout << "ehhh bark bark!" << std::endl;
-        }
-
-};
-
-class Husky: public Dog{
-
-};
+//dynamic programming
 
 int main(){
-  
-    Husky huzz;
 
-    huzz.bark();
+    // computing fibinacci number.
+
+    int computeFibonacci(int number){
+        int result = 0;
+
+        
+        
+        return result;
+    }
 
     return 0;
 }
