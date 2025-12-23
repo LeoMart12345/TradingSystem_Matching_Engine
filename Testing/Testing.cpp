726496749d20e26d4e1475350f74bb8fddac1768
Leo Martinez <l.copemartinez1@universityofgalway.ie>
2025-12-23 12:49:33 +0000

    cleanup

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -1,21 +1,3 @@
 #include <iostream>
-#include <array>
-#include <vector>
-#include <memory>
 
-//dynamic programming
 
-int main(){
-
-    // computing fibinacci number.
-
-    int computeFibonacci(int number){
-        int result = 0;
-
-        
-        
-        return result;
-    }
-
-    return 0;
-}
