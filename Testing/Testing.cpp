225e8fc3031c2f20d9bc685c47e65c7983d73e93
Leo Martinez <leomartinezhz@gmail.com>
2026-02-14 00:36:35 +0000

    testing new c++ 23 printf function

---
+++ Testing/Testing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+int main(){
+    std::printf("HELLO PRINTF ");
+
+
+
+
+    // size of 10 all all values will 0. 
+    std::array<int, 10> nums{4, 3, 1, 3,  1};
+
+}
