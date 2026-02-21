5875d6ac54b070a99a556d1612e9e295f9d6ad4e
Leo Martinez <leomartinezhz@gmail.com>
2026-02-21 00:19:19 +0000

    TraderClient refactored into a class

--- TradingSystem/Network Servers/Client_Test.cpp
+++ TradingSystem/Network Servers/Client_Test.cpp
@@ -9,101 +9,114 @@
 #include <boost/asio.hpp>
 #include "UDPMarketDataReceiver.hpp"
 #include "TCPOrderSession.hpp"
+#include "./Network Servers/MarketData.hpp"
 
-int main() {    
-    //UDP handler
+class TraderClient{
+    private: 
     MarketData marketData;
-    UDPMarketDataReceiver udpReceiver(marketData);
-    udpReceiver.start();
-    //TCP handler
-    TCPOrderSession TCPOrderEntry;
-    TCPOrderEntry.connect("127.0.0.1", 5555);
+    UDPMarketDataReceiver udpReceiver;
+    TCPOrderSession TCPsession;
+    u_int64_t nextClientId;
 
-    static uint64_t nextClientId = 1000;
+    public:
+    TraderClient(const std::string& host, int tcpPort) : 
+    udpReceiver(marketData)
+    {
+        TCPsession.connect(host, tcpPort);
+        udpReceiver.start();
+    }
 
-    bool running = true;
-    while (running) {
-        system("clear");
-        MarketDataSnapshot snapshot = marketData.get();
-        // Market data that will be updated via UDP multicasts
-        std::cout << "=== TRADING TERMINAL ===\n\n";
-        std::cout << "+-----------+----------+----------+\n";
-        std::cout << "| APPL      |   BID    |   ASK    |\n";
-        std::cout << "+-----------+----------+----------+\n";
-        std::cout << "| Price     | " << std::setw(8) << snapshot.bestBid << " | " << std::setw(8) << snapshot.bestAsk << " |\n";
-        std::cout << "| Volume    | " << std::setw(8) << snapshot.bidVolume << " | " << std::setw(8) << snapshot.askVolume << " |\n";
-        std::cout << "+-----------+----------+----------+\n\n";
 
-        std::cout << "1. Place Order\n";
-        std::cout << "2. Cancel Order\n";
-        std::cout << "3. Refresh\n";
-        std::cout << "4. Exit\n\n";
-        int choice;
-        std::cin >> choice;
-        
-        if (choice == 1) {
-            system("clear");
-            std::cout << "=== PLACE ORDER ===\n\n";
-            std::string tickerSymbol, side, qty, price;
-            std::cout << "Ticker Symbol: ";
-            std::cin >> tickerSymbol;
-            std::cout << "Side (BUY/SELL): ";
-            std::cin >> side;
-            std::cout << "Quantity: ";
-            std::cin >> qty;
-            std::cout << "Price: ";
-            std::cin >> price;
-            std::cout << "\nSending: " << side << " " << tickerSymbol 
-                      << " " << qty << " @ " << price << "\n";
-            // standardise the inputs
-            std::transform(side.begin(), side.end(), side.begin(), ::toupper);
+    u_int64_t placeOrder(const std::string& ticker, const std::string& side, uint64_t qty, uint64_t priceInCents) {
+        Price orderPrice(priceInCents);
+        Order order((side == "BUY") ? Bid : Ask, qty, ticker, 0, orderPrice);
+        return TCPsession.placeOrder(order);
+    }
 
-            // Making price object:
-            u_int64_t priceInCents = std::stoull(price);
-            Price orderPrice(priceInCents); 
-            // Make Order Object
-            Order order(
-                (side == "BUY") ? Bid : Ask,
-                std::stoull(qty),
-                tickerSymbol,
-                0,
-                orderPrice
-            );
+    std::string cancelOrder(uint64_t orderId) {
+        return TCPsession.cancelOrder(orderId);
+    }
 
-            // use TCPOrderSession instead of socket directly
-            uint64_t assignedId = TCPOrderEntry.placeOrder(order);
-            std::cout << "Order accepted with ID: " << assignedId << std::endl;
-            std::cout << "\nPress Enter...";
-            std::cin.ignore();
-            std::cin.get();
-        }
-        else if (choice == 2) {
-            system("clear");
-            std::string orderId;
-            std::cout << "Order ID: ";
-            std::cin >> orderId;
-
-            // use TCPOrderSession instead of socket directly
-            std::string response = TCPOrderEntry.cancelOrder(std::stoull(orderId));
-            std::cout << "Server Response: " << response << std::endl;
-
-            std::cout << "\nPress Enter...";
-            std::cin.ignore();
-            std::cin.get();
-        }
-        else if (choice == 3) {
-            //refresh
-        }else if (choice == 4) {
-            running = false;
-            std::cout << "\nExiting...\n";
-        }
-        else {
-            std::cout << "\nInvalid choice!\n";
-            std::cout << "Press Enter...";
-            std::cin.ignore();
-            std::cin.get();
-        }
+    MarketDataSnapshot getMarketData() {
+        return marketData.get();
     }
     
-    return 0;
+    void runTerminal() {
+        bool running = true;
+        while (running) {
+            system("clear");
+            MarketDataSnapshot snapshot = getMarketData();
+            std::cout << "=== TRADING TERMINAL ===\n\n";
+            std::cout << "+-----------+----------+----------+\n";
+            std::cout << "| APPL      |   BID    |   ASK    |\n";
+            std::cout << "+-----------+----------+----------+\n";
+            std::cout << "| Price     | " << std::setw(8) << snapshot.bestBid << " | " << std::setw(8) << snapshot.bestAsk << " |\n";
+            std::cout << "| Volume    | " << std::setw(8) << snapshot.bidVolume << " | " << std::setw(8) << snapshot.askVolume << " |\n";
+            std::cout << "+-----------+----------+----------+\n\n";
+
+            std::cout << "1. Place Order\n";
+            std::cout << "2. Cancel Order\n";
+            std::cout << "3. Refresh\n";
+            std::cout << "4. Exit\n\n";
+            int choice;
+            std::cin >> choice;
+
+            if (choice == 1) {
+                system("clear");
+                std::cout << "=== PLACE ORDER ===\n\n";
+                std::string tickerSymbol, side, qty, price;
+                std::cout << "Ticker Symbol: ";
+                std::cin >> tickerSymbol;
+                std::cout << "Side (BUY/SELL): ";
+                std::cin >> side;
+                std::cout << "Quantity: ";
+                std::cin >> qty;
+                std::cout << "Price: ";
+                std::cin >> price;
+                std::cout << "\nSending: " << side << " " << tickerSymbol 
+                        << " " << qty << " @ " << price << "\n";
+
+                std::transform(side.begin(), side.end(), side.begin(), ::toupper);
+
+                uint64_t assignedId = placeOrder(tickerSymbol, side, std::stoull(qty), std::stoull(price));
+                std::cout << "Order accepted with ID: " << assignedId << std::endl;
+                std::cout << "\nPress Enter...";
+                std::cin.ignore();
+                std::cin.get();
+            }
+            else if (choice == 2) {
+                system("clear");
+                std::string orderId;
+                std::cout << "Order ID: ";
+                std::cin >> orderId;
+
+                std::string response = cancelOrder(std::stoull(orderId));
+                std::cout << "Server Response: " << response << std::endl;
+
+                std::cout << "\nPress Enter...";
+                std::cin.ignore();
+                std::cin.get();
+            }
+            else if (choice == 3) {
+                // To Refresh: it just repeats.
+            }
+            else if (choice == 4) {
+                running = false;
+                std::cout << "\nExiting...\n";
+            }
+            else {
+                std::cout << "\nInvalid choice!\n";
+                std::cout << "Press Enter...";
+                std::cin.ignore();
+                std::cin.get();
+            }
+        }
+}
+
+};
+
+int main() {
+    std::string host = "127.0.0.1";
+    TraderClient trader(host, 5555);
+    trader.runTerminal();
 }
