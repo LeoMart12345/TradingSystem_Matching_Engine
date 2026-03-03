e7a9c291ba034b1983696bc4969c4bd1347148b0
leomart12345 <leomartinezhz@gmail.com>
2026-03-03 21:33:31 +0000

    fixing double free: added mutext to object pool

--- TradingSystem/Matching_Engine/OrderBook/ObjectPool.hpp
+++ TradingSystem/Matching_Engine/OrderBook/ObjectPool.hpp
@@ -3,12 +3,15 @@
 #include <array>
 #include <iostream> 
 #include <stack>
+#include <mutex>
 
 template<typename T,size_t Size> 
 class ObjectPool{
 private: 
     std::array<T, Size> pool;
     std::stack<size_t> freeStack;
+    std::mutex poolMutex;
+
 public: 
     ObjectPool(){
         for(size_t i=0; i < Size; i++){
@@ -17,13 +20,15 @@ public:
     }
     
     T* acquire(){
-        if(freeStack.empty())throw std::runtime_error("freeStack is full: there are no objects to acquire");
+      std::lock_guard<std::mutex> lock(poolMutex);
+      if(freeStack.empty())throw std::runtime_error("freeStack is full: there are no objects to acquire");
         size_t slot = freeStack.top(); // use top as pop returns void.
         freeStack.pop();
         return &pool[slot]; // get the memory address of the pool object that is next in the "freeStack" freeList
     }
-    
+     
     void release(T* obj){
+        std::lock_guard<std::mutex> lock(poolMutex);
         size_t slot = obj - &pool[0];
         if(slot >= Size){
             throw std::runtime_error("ObjectPool::release received pointer outside pool bounds");
